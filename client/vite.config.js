1fe61c963f7b5910c05744a0c59f7bb55375115c
Tim Quattrochi <timq82@gmail.com>
2023-05-31 23:57:35 -0400

    changed jsxRunTime

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   mode: "production",
-  plugins: [react({ jsxRuntime: "classic" })],
+  plugins: [react({ jsxRuntime: "automatic" })],
 });
