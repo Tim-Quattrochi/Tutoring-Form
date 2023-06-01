dabf54ca902742d9562b77bd59af3269b1803d93
Tim Quattrochi <timq82@gmail.com>
2023-05-31 23:46:15 -0400

    changed vite config for dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   mode: "production",
   plugins: [react({ jsxRuntime: "classic" })],
-  build: {
-    outDir: "client/dist",
-  },
 });
