c95befcefddea2dcd856a88b7e940f6f30932e30
Tim Quattrochi <timq82@gmail.com>
2023-06-01 00:28:39 -0400

    added path

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db");
 
 const app = express();
