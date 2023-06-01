659a029cb5cd35e100645b0f8417b7c0f9b589fd
Tim Quattrochi <timq82@gmail.com>
2023-05-31 23:31:35 -0400

    netlify deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   mode: "production",
   plugins: [react({ jsxRuntime: "classic" })],
+  build: {
+    outDir: "client/dist",
+  },
 });
